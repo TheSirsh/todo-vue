cc9b25763482466b3d1466b8b5f49ef66f4a12b6
TheSirsh <sirsh@bk.ru>
2023-06-02 15:10:45 +0300

    feat: add server data array

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -21,12 +21,15 @@ export default {
   name: 'App',
   data() {
     return {
-      todos: [
-        {id: 1, title: "Купить хлеб", completed: false},
-        {id: 2, title: "Купить масло", completed: false},
-        {id: 3, title: "Купить пиво", completed: false},
-      ],
+      todos: [],
     }
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
+      .then(response => response.json())
+      .then(json => {
+        this.todos = json
+      })
   },
   methods: {
     removeTodo(id) {
