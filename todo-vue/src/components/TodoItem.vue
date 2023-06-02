210f2d2335416cd4fd0299657e6928ceb067547c
TheSirsh <sirsh@bk.ru>
2023-06-02 14:20:41 +0300

    feat: update todoitem

--- todo-vue/src/components/TodoItem.vue
+++ todo-vue/src/components/TodoItem.vue
@@ -1,6 +1,13 @@
 <template>
   <li>
-    {{ todo.title }}
+    <span>
+      <input type="checkbox">
+      <strong>{{ todo.id }}</strong>
+      {{ todo.title }}
+    </span>
+    <button>
+      &times;
+    </button>
   </li>
 </template>
 
